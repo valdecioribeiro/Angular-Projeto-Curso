f2d4d06b9323fc09946cf1dec2656871dd8c9126
Valdecio Borges Ribeiro <valdecio.ribeiro@serpro.gov.br>
2023-09-04 12:07:56 -0300

    Commit inicial

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  
+  recipes = []
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
