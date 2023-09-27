b1c1aeb622085ff606ffb5d46bc1ef1aa5297cd8
Valdecio Ribeiro <valdecio@gmail.com>
2023-09-27 15:40:37 -0300

    Últimas modificações

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe(
+      "A teste recipe", 
+      "Isso é um teste", 
+      "https://ciadereceitas.com.br/wp-content/uploads/2022/09/receita-de-bem-casado.png")
+  ];
+
+  constructor() {
+
+  }
   
   ngOnInit(): void {
     throw new Error('Method not implemented.');
