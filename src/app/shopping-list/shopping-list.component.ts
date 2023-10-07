5a9d31815d8f124f4470db65c92182fd1f724ef0
Valdecio Ribeiro <valdecio@gmail.com>
2023-10-06 21:59:23 -0300

    Últimas modificações

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredientes: Ingredient [] = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Tomatoes", 10)
+  ];
+
+  ngOnInit(){
+
+  }
 
 }
